0c411090ed190723385c676e5d3e5757eab88d64
John <developer.johnpersano@gmail.com>
2017-09-25 08:39:11 -0700

    Bootstrap import and home page development

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'page#home'
+
+  # Static pages
+  PageController.action_methods.each do |action|
+    get "/#{action}", to: "page##{action}", as: "#{action}_page"
+  end
 end
